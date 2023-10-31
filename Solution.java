fe04d53766a239b0a06f53065664c5300b94f981
F93AB7X <joseph.schmitt1@fiserv.com>
2023-10-31 14:14:06 -0400

    submission algorithm hours number 5

--- Solution.java
+++ Solution.java
@@ -1,12 +1,41 @@
+import java.util.HashMap;
+
+import javax.xml.transform.Source;
+
 class Solution {
-    public static char mostFrequentChar(String s) {
-      
-      return;
-    }
-  
-    public static void main(String[] args) {
-      // Try out your test cases:
-      System.out.println(Solution.mostFrequentChar("bookeeper")); // -> 'e'
+  public static char mostFrequentChar(String s) {
+
+    HashMap<Character, Integer> mp = new HashMap<>();
+
+    char ans = 0;
+
+    int cnt = 0;
+
+    for (int i = 0; i < s.length(); i++) {
+
+      char c = s.charAt(i);
+      mp.put(c, mp.getOrDefault(c, 0) + 1);
+
+      // use less then or EQUAL TO to get first occuring value
+      if (cnt <= mp.get(c)) {
+        ans = c;
+        cnt = mp.get(c);
+      }
     }
 
+    return ans;
   }
+
+  public static void main(String[] args) {
+
+    System.out.println(mostFrequentChar("bookeeper")); // -> 'e'
+
+    System.out.println(mostFrequentChar("david")); // -> 'd'
+
+    System.out.println(mostFrequentChar("mississippi")); // -> 'i'
+
+    System.out.println(mostFrequentChar("eleventennine")); // -> 'e'
+
+  }
+
+}

4b82c0048920629d0b332a7688398c39d1454301
F93AB7X <joseph.schmitt1@fiserv.com>
2023-10-31 15:02:38 -0400

    correct algo

--- Solution.java
+++ Solution.java
@@ -1,42 +1,34 @@
 import java.util.HashMap;
 
-import javax.xml.transform.Source;
-
 class Solution {
   public static char mostFrequentChar(String s) {
-
     HashMap<Character, Integer> mp = new HashMap<>();
-
-    char ans = 0;
-
-    int cnt = 0;
-
     for (int i = 0; i < s.length(); i++) {
-
       char c = s.charAt(i);
-      mp.put(c, mp.getOrDefault(c, 0) + 1);
-
-      // Use less then or equal to because last value will always 
-      //technically be the first value which is the criteria.
-      if (cnt <= mp.get(c)) {
-        System.out.println(c);
-        ans = c;
-        cnt = mp.get(c);
+      Integer val = mp.get(c);
+      if (val != null) {
+        mp.put(c, val + 1);
+      } else {
+        mp.put(c, 1);
       }
     }
-
+    char ans = 'a';
+    int mx = -1;
+    for (int i = 0; i < s.length(); ++i) {
+      if (mp.get(s.charAt(i)) > mx) {
+        mx = mp.get(s.charAt(i));
+        ans = s.charAt(i);
+      }
+    }
     return ans;
   }
 
   public static void main(String[] args) {
-
-    System.out.println(mostFrequentChar("bookeeper")); // -> 'e'
-
-    System.out.println(mostFrequentChar("david")); // -> 'd'
-
-    System.out.println(mostFrequentChar("mississippi")); // -> 'i'
-
-    System.out.println(mostFrequentChar("eleventennine")); // -> 'e'
+    // Try out your test cases:
+    System.out.println(Solution.mostFrequentChar("bookeeper")); // -> 'e'
+    System.out.println(Solution.mostFrequentChar("david")); // -> 'd'
+    System.out.println(Solution.mostFrequentChar("mississippi")); // -> 'i'
+    System.out.println(Solution.mostFrequentChar("eleventennine")); // -> 'e'
 
   }
 
